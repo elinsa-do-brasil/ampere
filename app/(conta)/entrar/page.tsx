42be40316283ccd6f3c8d1a56b0377f4629aa20f
Raave Aires <developer@raave.me>
2025-12-18 11:26:07 -0300

    fix: corrige a importação do header feat: adiciona o aviso sobre o funcionamento do login com a microsoft

--- app/(conta)/entrar/page.tsx
+++ app/(conta)/entrar/page.tsx
@@ -4,27 +4,22 @@ import {
 } from "@/app/(conta)/components/card-content";
 
 import {
-  GithubOauthButton,
-  GoogleOauthButton,
   MicrosoftOauthButton,
 } from "@/components/auth/buttons/oauth-buttons";
 import { LoginForm } from "@/components/auth/forms/login";
 import { Or } from "@/components/auth/or";
-import { RaauthCardHeader } from "../components/raauth-card-header";
+import { AmperCardHeader } from "../components/card-header";
 import { CardFooter } from "../components/card-footer";
 
 export default function Page() {
   return (
     <>
-      <RaauthCardHeader login />
+      <AmperCardHeader login />
       <CardContent>
-        <div className="grid grid-cols-3 space-x-2">
-          <MicrosoftOauthButton />
-          <GoogleOauthButton />
-          <GithubOauthButton />
-        </div>
-        <Or />
         <LoginForm />
+        <Or texto="ou" />
+        <MicrosoftOauthButton />
+        <p className="text-center text-muted-foreground text-xs max-w-[80%] mx-auto">O login com a Microsoft só está disponível para funcionários da Amper Elinsa.</p>
       </CardContent>
       <CardFooter login />
     </>
