946682de99ca2a3caa2d797b937ce9d5b05e8616
Raave Aires <developer@raave.me>
2025-12-19 11:13:02 -0300

    fix: corrige um corpotamento inesperado do header, a saber, ao usar position sticky, o header desaparecia quando se abria um dropdown e havia algo passando trás do header

--- components/structure/header.tsx
+++ components/structure/header.tsx
@@ -9,7 +9,9 @@ import { Account } from "@/components/account/account";
 export function Header() {
   return (
     <>
-      <header className="sticky h-14 min-w-dvw max-w-dvw top-0 flex justify-center items-center bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60 border-b border-border z-50">
+      {/* Espaçador para compensar o header fixed */}
+      <div className="h-14" />
+      <header className="fixed h-14 min-w-dvw max-w-dvw top-0 left-0 flex justify-center items-center bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60 border-b border-border z-50">
         <nav className="container flex items-center justify-between">
           <Logo />
 
